487f54d96e3810b6dcf0636fb811e3d555b87a0c
wilburliu <wilburliu1123@gmail.com>
2017-01-13 14:04:15 -0500

    implement wage part

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113145432) do
+ActiveRecord::Schema.define(version: 20170113183638) do
 
   create_table "admins", force: :cascade do |t|
-    t.integer  "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
