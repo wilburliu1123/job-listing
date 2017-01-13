973c9c04a00837d5cd2363bbdce279a6c97e6c4f
wilburliu <wilburliu1123@gmail.com>
2017-01-13 13:53:20 -0500

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
-  
+
   def index
     @jobs= Job.all
   end
@@ -37,6 +37,6 @@ class Admin::JobsController < ApplicationController
   end
   private
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
