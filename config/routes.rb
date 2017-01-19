38ee8da0333b6d2aae605590caa040b21156d392
wilburliu <wilburliu1123@gmail.com>
2017-01-19 18:16:09 -0500

    modify user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users
+  devise_for :users, controllers:{session: 'users/sessions'}
   namespace :admin do
     resources :jobs do
 
